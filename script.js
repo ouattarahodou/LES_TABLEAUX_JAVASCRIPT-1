6b2d302de2d7aea21478ea11a0381bd08d31eff2
ouattarahodou <83280873+ouattarahodou@users.noreply.github.com>
2022-03-12 23:03:01 +0100

    ajout des fichiers js

---
+++ script.js
@@ -0,0 +1,26 @@
+// LES TABLEUX
+
+var age = 14;
+var fruits = ["orange"]
+fruits.push("pomme")
+fruits.push("citron")
+fruits.push("banane")
+fruits.push("poire")
+fruits.unshift()
+fruits.splice(0, 1,"bobo")
+fruits.length
+
+// FRUITS.PUSH() pour ajout d'element vers la fin
+// FRUITS.UNSHIFT() pour ajout d'un element au debut
+// FRUITS[] pour retier un element
+// FRUITS.pop() pour retirer le dernier element
+// FRUITS.length pour voir ce qui est dans le tableu 
+// FRUITS.SHIFT() pour retirer le premier element
+// FRUITS.splice(a ramplacer,a ajouter,"valer") pour modifier une faleure 
+
+// LES TABLEUX 2 
+var  names = ["Alice","Nicolas","Nael","Ismael"]
+string = names.join(' . ')
+
+
+
